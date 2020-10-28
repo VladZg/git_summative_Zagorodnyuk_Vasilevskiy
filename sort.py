10445eedc4d38359b7ed18941d0171f148020fbe
VladZg <71943700+VladZg@users.noreply.github.com>
2020-10-28 11:48:02 +0300

    Merge sorts

--- sort.py
+++ sort.py
@@ -31,3 +31,11 @@ def sort_shaker(s):
         start_index = start_index + 1
 
     return(s)
+
+def sort_bubble(arr):
+    n = len(arr)
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
